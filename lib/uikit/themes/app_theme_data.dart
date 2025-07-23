b97baadbaba7043f902a2d3740f58df6750f55ba
m00nlight1 <pavushas.work@gmail.com>
2025-07-23 14:55:44 +0300

    Add AppThemeData

---
+++ lib/uikit/themes/app_theme_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:interesting_places/uikit/themes/colors/app_color_theme.dart';
+import 'package:interesting_places/uikit/themes/text/app_text_theme.dart';
+
+abstract class AppThemeData {
+  static const _lightColorTheme = AppColorTheme.light();
+  static final _textTheme = AppTextTheme.base();
+
+  static final lightTheme = ThemeData(
+    extensions: [_lightColorTheme, _textTheme],
+    brightness: Brightness.light,
+    primaryColor: _lightColorTheme.primary,
+    scaffoldBackgroundColor: _lightColorTheme.primary,
+    appBarTheme: AppBarTheme(
+      backgroundColor: _lightColorTheme.primary,
+      elevation: 0,
+      surfaceTintColor: Colors.transparent,
+      iconTheme: IconThemeData(color: _lightColorTheme.icon),
+      titleTextStyle: _textTheme.titleLarge,
+    ),
+    dialogTheme: DividerThemeData(color: _lightColorTheme.divider),
+    sliderTheme: SliderThemeData(
+      trackHeight: 2,
+      activeTrackColor: _lightColorTheme.accent,
+      inactiveTrackColor: _lightColorTheme.inactive,
+      activeTickMarkColor: Colors.transparent,
+      inactiveTickMarkColor: Colors.transparent,
+      thumbColor: _lightColorTheme.primary,
+      overlayShape: SliderComponentShape.noOverlay,
+      tickMarkShape: SliderTickMarkShape.noTickMark,
+      showValueIndicator: ShowValueIndicator.never,
+      padding: EdgeInsets.zero,
+    ),
+  );
+}
