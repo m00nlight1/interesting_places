bd00dc5446925560ab68a2dc14b24a1d111ae191
m00nlight1 <pavushas.work@gmail.com>
2025-07-26 00:15:52 +0300

    Provide PlacesRepository into places dependencies

--- lib/features/places/di/places_dependencies.dart
+++
@@ -1,9 +0,0 @@
-import 'package:interesting_places/features/places/ui/screens/places_model.dart';
-import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
-
-abstract class PlacesDependencies {
-  static List<SingleChildWidget> providers() {
-    return [Provider<IPlacesModel>(create: (_) => PlacesModel())];
-  }
-}
---
+++ lib/features/places/ui/places_dependencies.dart
@@ -0,0 +1,32 @@
+import 'package:interesting_places/api/service/api_client.dart';
+import 'package:interesting_places/features/common/converters/place_converter.dart';
+import 'package:interesting_places/features/common/converters/place_type_converter.dart';
+import 'package:interesting_places/features/common/domain/repositories/i_favorites_repository.dart';
+import 'package:interesting_places/features/places/data/repositories/places_repository.dart';
+import 'package:interesting_places/features/places/domain/repositories/i_places_repository.dart';
+import 'package:interesting_places/features/places/ui/screens/places_model.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+abstract class PlacesDependencies {
+  static List<SingleChildWidget> providers() {
+    return [
+      Provider<IPlacesRepository>(
+        create:
+            (context) => PlacesRepository(
+              apiClient: context.read<ApiClient>(),
+              placeDtoToEntityConverter: PlaceDtoToEntityConverter(
+                placeTypeConverter: PlaceTypeDtoToEntityConverter(),
+              ),
+            ),
+      ),
+      Provider<IPlacesModel>(
+        create:
+            (context) => PlacesModel(
+              placesRepository: context.read<IPlacesRepository>(),
+              favoritesRepository: context.read<IFavoritesRepository>(),
+            ),
+      ),
+    ];
+  }
+}
