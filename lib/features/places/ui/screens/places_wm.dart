9f104b538c59687da475f9244ddf1880cd2dd88f
m00nlight1 <pavushas.work@gmail.com>
2025-07-26 00:21:23 +0300

    Updated places WM

--- lib/features/places/ui/screens/places_wm.dart
+++ lib/features/places/ui/screens/places_wm.dart
@@ -1,41 +1,46 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:interesting_places/features/common/domain/entities/place_entity.dart';
 import 'package:interesting_places/features/common/domain/repositories/i_favorites_repository.dart';
-import 'package:interesting_places/features/common/models/place.dart';
 import 'package:interesting_places/features/place_detail/ui/screens/place_detail_screen_builder.dart';
+import 'package:interesting_places/features/places/domain/enities/places_state.dart';
 import 'package:interesting_places/features/places/ui/screens/places_model.dart';
 
 abstract interface class IPlacesWM {
-  ValueListenable<List<Place>> get placesStateListenable;
+  ValueListenable<PlacesState> get placesStateListenable;
 
   void dispose();
 
-  void onPlacePressed(BuildContext context, Place place);
+  void onPlacePressed(BuildContext context, PlaceEntity place);
 
-  void onLikePressed(Place place);
+  void onLikePressed(PlaceEntity place);
 
-  bool isFavorite(Place place);
+  bool isFavorite(PlaceEntity place);
+
+  Future<void> loadPlaces();
 }
 
 class PlacesWM implements IPlacesWM {
   final IPlacesModel _model;
   final IFavoritesRepository _favoritesRepository;
 
-  PlacesWM(this._model, this._favoritesRepository);
+  PlacesWM(this._model, this._favoritesRepository) {
+    _model.getPlaces();
+  }
 
   @override
   void dispose() => _model.dispose();
 
   @override
-  bool isFavorite(Place place) => _favoritesRepository.isFavorite(place);
+  bool isFavorite(PlaceEntity place) => _favoritesRepository.isFavorite(place);
 
   @override
-  void onLikePressed(Place place) {
+  void onLikePressed(PlaceEntity place) {
     _favoritesRepository.toggleFavorite(place);
   }
 
   @override
-  void onPlacePressed(BuildContext context, Place place) {
+  void onPlacePressed(BuildContext context, PlaceEntity place) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) => PlaceDetailScreenBuilder(place: place),
@@ -44,6 +49,9 @@ class PlacesWM implements IPlacesWM {
   }
 
   @override
-  ValueListenable<List<Place>> get placesStateListenable =>
+  ValueListenable<PlacesState> get placesStateListenable =>
       _model.placesStateListenable;
+
+  @override
+  Future<void> loadPlaces() => _model.getPlaces();
 }
