06868cc4cf82a1f7a4e9cfca94e651ba43e46da1
m00nlight1 <pavushas.work@gmail.com>
2025-07-25 20:50:57 +0300

    Add ApiClient provider with Dio configuration to app dependencies

--- lib/features/app/di/app_dependencies.dart
+++ lib/features/app/di/app_dependencies.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:interesting_places/api/service/api_client.dart';
 import 'package:interesting_places/features/common/data/repositories/favorites_repository.dart';
 import 'package:interesting_places/features/common/domain/repositories/i_favorites_repository.dart';
 import 'package:provider/provider.dart';
@@ -5,7 +7,19 @@ import 'package:provider/single_child_widget.dart';
 
 abstract class AppDependencies {
   static List<SingleChildWidget> providers() {
+    const timeout = Duration(seconds: 30);
+
+    final dio = Dio();
+
+    dio.options
+      ..baseUrl = 'http://109.73.206.134:8888/api/'
+      ..connectTimeout = timeout
+      ..receiveTimeout = timeout
+      ..sendTimeout = timeout;
+
+    final apiClient = ApiClient(dio);
     return [
+      Provider<ApiClient>(create: (_) => apiClient),
       Provider<IFavoritesRepository>(create: (_) => FavoritesRepository()),
     ];
   }
